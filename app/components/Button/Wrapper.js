2a0e3a4005a23dff0bfed8b128ecb55af41555f6
Melissa Thompson <mthompson828@gmail.com>
2018-11-10 14:50:01 -0500

    home page buttons

---
+++ app/components/Button/Wrapper.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.button`
+  padding: 16px 24px;
+  color: white;
+  background-color: #6471df;
+  margin: 20px;
+  display: block;
+  border-radius: 4px;
+`;
+
+export default Wrapper;
---
+++ app/containers/HomePage/Layout.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  margin: 0 auto;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Layout;
