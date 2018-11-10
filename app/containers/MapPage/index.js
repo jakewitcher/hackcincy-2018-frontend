dabf1f7c2343f3ed06e52cb3a02935bc0658a8bf
Jake Witcher <jjwitcher09@gmail.com>
2018-11-10 15:41:54 -0500

    add map component file

--- app/containers/MapPage/index.js
+++ app/containers/MapPage/index.js
@@ -10,33 +10,14 @@
  */
 
 import React from 'react';
-import GoogleMapReact from 'google-map-react';
+import Maps from 'components/Map'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-/* eslint-disable react/prefer-stateless-function */
+
 export default class MapPage extends React.PureComponent {
-  static defaultProps = {
-    center: {
-      lat: 39.103119,
-      lng: -84.512016,
-    },
-    zoom: 14,
-  };
-
   render() {
     return (
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyD_l96qafgvKgE1b-cLSw4Zuh1h-bapTqI' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={39.103119}
-            lng={-84.512016}
-            text="Cincinnati"
-          />
-        </GoogleMapReact>
+      <div>
+        <Maps />
       </div>
     );
   }
